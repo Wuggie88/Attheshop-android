f441be0b2fc2f1e44fd0859efab051e7389c187b
Wuggie <wuggie88@yahoo.co.uk>
2021-05-10 13:05:55 +0200

    added button listener

--- app/src/main/java/com/example/attheshop/MainActivity.kt
+++ app/src/main/java/com/example/attheshop/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.attheshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val customerButton = findViewById<Button>(R.id.CustomerButton)
+
+        customerButton.setOnClickListener {
+            val intent = Intent(this, Kunder::class.java)
+
+            startActivity(intent)
+        }
     }
 }
