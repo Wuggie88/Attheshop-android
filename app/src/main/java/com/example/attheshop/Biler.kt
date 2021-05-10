92d7853d5eae5f16a32af3a2b71f30ca5a0158df
Wuggie <wuggie88@yahoo.co.uk>
2021-05-10 15:10:43 +0200

    Added lifecycle events

--- app/src/main/java/com/example/attheshop/Biler.kt
+++ app/src/main/java/com/example/attheshop/Biler.kt
@@ -18,6 +18,43 @@ class Biler : AppCompatActivity() {
 
             startActivity(intent)
         }
-        
+
     }
+
+
+    //onStart is is called when activity is visible to the user.
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+    //onResume is called when the activity is going to the foreground.
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    //onRestart is called when you stop the activity after it has already been running.
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    //onPause is called when an activity goes to the background.
+    override fun onPause() {
+        super.onPause()
+
+    }
+
+    //onStop is called when the activity is no longer visible but can still save data.
+    override fun onStop() {
+        super.onStop()
+
+    }
+
+    //onDestroy is called as the last thing in the activity right before it's destroyed.
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
 }
