1fa8192b03bbfe32169d11f30ac20160f7de7a6a
Wuggie <wuggie88@yahoo.co.uk>
2021-05-10 13:38:59 +0200

    Added back button to activity fabs

--- app/src/main/java/com/example/attheshop/Biler.kt
+++ app/src/main/java/com/example/attheshop/Biler.kt
@@ -1,5 +1,6 @@
 package com.example.attheshop
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -10,11 +11,11 @@ class Biler : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biler)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/attheshop/Kunder.kt
+++ app/src/main/java/com/example/attheshop/Kunder.kt
@@ -10,7 +10,6 @@ class Kunder : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kunder)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
