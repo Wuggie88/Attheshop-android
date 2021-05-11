8377ae174334bbdedb71e011005ac6e7fa852e92
Wuggie <wuggie88@yahoo.co.uk>
2021-05-11 13:42:59 +0200

    renamed an activity to reservedele

--- app/src/main/java/com/example/attheshop/Reservedele.kt
+++ app/src/main/java/com/example/attheshop/Reservedele.kt
@@ -3,14 +3,13 @@ package com.example.attheshop
 import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 class Reservedele : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_senest_aabne)
+        setContentView(R.layout.activity_reservedele)
 
         //Sets the button with "fab" as ID, to go back to the MainActivity
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
