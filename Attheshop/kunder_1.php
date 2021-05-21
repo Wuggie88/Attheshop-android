23b6b354c6fa910e05a8515714d1e86ec511d05f
Anders Gaarsdal <angaa19@student.sdu.dk>
2021-05-21 15:30:22 +0200

    Extra

---
+++ Attheshop/kunder_1.php
@@ -0,0 +1,37 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "attheshop";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+    
+// Check connection
+if ($conn->connect_error) {
+    die("NEW Connection failed: " . $conn->connect_error);
+}
+
+ //response array 
+ $response = array();     
+    
+// Select table
+$sql = "SELECT Navn, Nummer FROM kunder";
+$result = $conn->query($sql);
+
+     
+if ($result->num_rows > 0) {
+    
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    
+        $response[] = $row;
+    }
+     echo json_encode($response);
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
